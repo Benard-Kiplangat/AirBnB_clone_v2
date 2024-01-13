98476f2bdb2cd47e9a1ba7a2c8fe48fd6e3ab546
benard Kiplangat <benardkiplangat97@gmail.com>
2024-01-13 11:17:34 +0300

    Removed root from the nginx config and added a reload to put the changes to take effect

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # A scripts that sets up our web server for our static content in nginx
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get install -y nginx
 
 sudo mkdir -p /data/
@@ -10,15 +11,15 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo echo "Hello World!" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -fR ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 sudo printf %s "server {
      listen      80 default_server;
      listen      [::]:80 default_server;
-     root        /data/web_static/current/;
      location /hbnb_static/ {
      			alias /data/web_static/current/;
 		}
      index       index.html index.htm;
 }
 " > /etc/nginx/sites-available/default
+sudo nginx -s reload
