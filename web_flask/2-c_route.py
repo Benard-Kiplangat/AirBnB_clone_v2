6df7e8729e4843a9a419f7cd1dc1d15deedcd21f
benard Kiplangat <benardkiplangat97@gmail.com>
2024-01-28 16:07:09 +0300

    Fixed PEP 8 errors on pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
-Prints hello, HBNB! on route "/" and HBNB on route "/hbnb"
+Prints Hello, HBNB! on route "/", HBNB on route "/hbnb", and
+C is + text variable passed to route
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Returns Hello, HBNB!"""
     return "Hello, HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_route():
-    """Returns, HBNB!"""
+    """Returns HBNB"""
     return "HBNB"
 
-@app.route("/c/<cvariable>", strict_slashes=False)
+
+@app.route("/c/<string:cvariable>", strict_slashes=False)
 def hbnb_c_route(cvariable):
-    """Returns, HBNB!"""
+    """Returns C is + text passed"""
     return "C is {}".format(cvariable.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
 """
-Prints hello, HBNB! on route "/" and HBNB on route "/hbnb"
+Prints hello, HBNB! on route "/", HBNB on route "/hbnb",
+C is + text passed, and Python + text passed or is cool
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Returns Hello, HBNB!"""
     return "Hello, HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_route():
-    """Returns, HBNB!"""
+    """Returns HBNB"""
     return "HBNB"
 
-@app.route("/c/<cvariable>", strict_slashes=False)
+
+@app.route("/c/<string:cvariable>", strict_slashes=False)
 def hbnb_c_route(cvariable):
-    """Returns, HBNB!"""
+    """Returns C is + text passed"""
     return "C is {}".format(cvariable.replace('_', ' '))
 
+
 @app.route('/python', strict_slashes=False)
-@app.route("/python/<pvariable>", strict_slashes=False)
+@app.route("/python/<string:pvariable>", strict_slashes=False)
 def hbnb_p_route(pvariable="is cool"):
-    """Returns, HBNB!"""
+    """Returns Python + text passed or is cool"""
     return "Python {}".format(pvariable.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
