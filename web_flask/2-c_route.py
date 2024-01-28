60df7d9ea91ba33ead2b8785fa73ee922af8d3a0
benard Kiplangat <benardkiplangat97@gmail.com>
2024-01-28 16:19:05 +0300

    Removed port and changed the format string

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,23 +9,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Returns Hello, HBNB!"""
     return "Hello, HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
     """Returns HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<string:cvariable>", strict_slashes=False)
+@app.route('/c/<string:cvariable>', strict_slashes=False)
 def hbnb_c_route(cvariable):
     """Returns C is + text passed"""
-    return "C is {}".format(cvariable.replace('_', ' '))
+    return "C is %s" % cvariable.replace('_', ' ')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
